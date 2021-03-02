eb03e4a0c2bf1a3181c199fafec000f44f0179f8
jquintanillam <jquintanillam@lmde>
2021-03-02 20:32:50 +0100

    ultimo comit

--- ClinicaVeterinaria/src/gestion/Mascota.java
+++ ClinicaVeterinaria/src/gestion/Mascota.java
@@ -2,11 +2,22 @@ package gestion;
 
 import java.io.Serializable;
 
+/**
+*
+* @author Jesus
+*/
 public class Mascota extends Animales implements Serializable{
 
 	private String nombre;
     private Persona duenyo;
-
+/**
+ * 
+ * @param ID
+ * @param nombre
+ * @param edad
+ * @param raza
+ * @param duenyo
+ */
     public Mascota(long ID, String nombre, int edad, String raza, Persona duenyo) {
     	this.setID(ID);
         this.nombre = nombre;
@@ -14,23 +25,40 @@ public class Mascota extends Animales implements Serializable{
         this.raza = raza;
         this.duenyo = duenyo;
     }
-
+/**
+ * 
+ * @return te da nombre 
+ */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * 
+     * @return te da nombre que es igual que el mismo nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * 
+     * @return te da duenyo
+     */
     public Persona getDuenyo() {
         return duenyo;
     }
 
+    /**
+     * 
+     * @return te da el resultado que es igual que el mismo duenyo
+     */
     public void setDuenyo(Persona duenyo) {
         this.duenyo = duenyo;
     }
-
+/**
+ * return te da el nombre edad raza duenyo
+ */
     @Override
     public String toString() {
         return ("Nombre: " + nombre + " Edad: " + edad + " Raza: " + raza + " Due�o: " + duenyo.toString());
--- ClinicaVeterinaria/src/gestion/Persona.java
+++ ClinicaVeterinaria/src/gestion/Persona.java
@@ -2,6 +2,10 @@ package gestion;
 
 import java.io.Serializable;
 
+/**
+*
+* @author Jesus
+*/
 public class Persona implements Serializable{
 	private static final long serialVersionUID = 1L;
     private String nombre;
@@ -9,6 +13,13 @@ public class Persona implements Serializable{
     private String dni;
     private int edad;
 
+    /**
+     * 
+     * @param nombre
+     * @param apellidos
+     * @param dni
+     * @param edad
+     */
     public Persona(String nombre, String apellidos, String dni, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -16,42 +27,82 @@ public class Persona implements Serializable{
         this.edad = edad;
     }
 
+    /**
+     * 
+     * @return devuelve nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * 
+     * @param nombre devuelve nombre que es el mismo nombre que el que hay
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * 
+     * @return devuelve apellidos
+     */
     public String getApellidos() {
         return apellidos;
     }
 
+    /**
+     * 
+     * @param apellidos
+     */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    /**
+     * 
+     * @return devuelve dni
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * 
+     * @param dni
+     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
+    /**
+     * 
+     * @return devuelve edad
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * 
+     * @param devuelve edad
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    /**
+     * 
+     * @param anyoactual
+     * @return devuelve una resta
+     */
     public int dameAnyoNacimiento(int anyoactual) {
         return (anyoactual - this.getEdad());
     }
     
+    /**
+     * return devuelve el nombre, apellidos , dni, edad.
+     */
     @Override
     public String toString() {
         return("Nombre: " + nombre + " Apellidos: " + apellidos + " DNI: " + dni + " Edad: " + edad);        
