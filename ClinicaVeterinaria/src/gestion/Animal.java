6d4c78ad18f182b2792ed4a440b97cae956d69c2
jquintanillam <jquintanillam@lmde>
2021-03-02 20:10:24 +0100

    Commit and push 3

---
+++ ClinicaVeterinaria/src/gestion/Animal.java
@@ -0,0 +1,52 @@
+package gestion;
+
+import java.io.Serializable;
+
+public class Animal {
+
+	private long ID;
+	protected int edad;
+	protected String raza;
+
+	public Animal() {
+		super();
+	}
+
+	public int getEdad() {
+	    return edad;
+	}
+
+	public void setEdad(int edad) {
+	    this.edad = edad;
+	}
+
+	public String getRaza() {
+	    return raza;
+	}
+
+	public void setRaza(String raza) {
+	    this.raza = raza;
+	}
+
+	private long getID() {
+		return ID;
+	}
+
+	protected void setID(long iD) {
+		this.ID = iD;
+	}
+
+	@Override
+	public boolean repira() {
+		//aqu� vendr�a el c�digo para la respiraci�n
+		return true;
+	}
+
+	@Override
+	public boolean come(float cantidad) {
+		//aqu� vendr�a el c�digo para ver lo que comen
+		return true;
+		
+	}
+
+}
